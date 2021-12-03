39faac8ac6270550148e2d6b2afdabaad615aa3b
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2021-12-03 17:32:20 +0000

    refactor: client operations now receive a context

--- internal/cli/client/create.go
+++ internal/cli/client/create.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"github.com/DuarteMRAlves/maestro/api/pb"
 	"github.com/DuarteMRAlves/maestro/internal/cli/resources"
-	"time"
 )
 
-func CreateAsset(asset *resources.AssetResource, addr string) error {
+func CreateAsset(
+	ctx context.Context,
+	asset *resources.AssetResource,
+	addr string,
+) error {
 	a := &pb.Asset{
 		Name:  asset.Name,
 		Image: asset.Image,
@@ -17,17 +20,16 @@ func CreateAsset(asset *resources.AssetResource, addr string) error {
 
 	c := pb.NewAssetManagementClient(conn)
 
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		time.Second)
-	defer cancel()
-
 	_, err := c.Create(ctx, a)
 
 	return err
 }
 
-func CreateStage(stage *resources.StageResource, addr string) error {
+func CreateStage(
+	ctx context.Context,
+	stage *resources.StageResource,
+	addr string,
+) error {
 	s := &pb.Stage{
 		Name:    stage.Name,
 		Asset:   stage.Asset,
@@ -39,17 +41,16 @@ func CreateStage(stage *resources.StageResource, addr string) error {
 
 	c := pb.NewStageManagementClient(conn)
 
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		time.Second)
-	defer cancel()
-
 	_, err := c.Create(ctx, s)
 
 	return err
 }
 
-func CreateLink(link *resources.LinkResource, addr string) error {
+func CreateLink(
+	ctx context.Context,
+	link *resources.LinkResource,
+	addr string,
+) error {
 	l := &pb.Link{
 		Name:        link.Name,
 		SourceStage: link.SourceStage,
@@ -61,12 +62,7 @@ func CreateLink(link *resources.LinkResource, addr string) error {
 	defer conn.Close()
 
 	c := pb.NewLinkManagementClient(conn)
-
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		time.Second)
-	defer cancel()
-
+	
 	_, err := c.Create(ctx, l)
 
 	return err
