82eb7fc3457cf1d6f3e0881ccde332bf5a0470b1
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2021-12-04 21:30:29 +0000

    refactor: refactored create asset command to options structure.

--- internal/cli/cmd/create/create_asset.go
+++ internal/cli/cmd/create/create_asset.go
@@ -2,10 +2,10 @@ package create
 
 import (
 	"context"
-	"fmt"
 	"github.com/DuarteMRAlves/maestro/internal/cli/client"
 	"github.com/DuarteMRAlves/maestro/internal/cli/resources"
 	"github.com/DuarteMRAlves/maestro/internal/cli/util"
+	"github.com/DuarteMRAlves/maestro/internal/errdefs"
 	"github.com/spf13/cobra"
 	"time"
 )
@@ -15,64 +15,83 @@ const (
 	imageUsage = "Docker image for the asset"
 )
 
-var createAssetOpts = &struct {
-	addr  string
-	files []string
-	image string
-}{}
+// CreateAssetOptions executes a create asset command
+type CreateAssetOptions struct {
+	addr string
 
+	name  string
+	image string
+}
+
+// NewCmdCreateAsset returns a new command that creates an asset from command
+// line arguments
 func NewCmdCreateAsset() *cobra.Command {
+	o := &CreateAssetOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "asset NAME",
 		Short: "create a new Asset",
-		Args:  cobra.MaximumNArgs(1),
-		Run:   runCreateAsset,
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			err := o.complete(args)
+			if err != nil {
+				util.WriteOut(cmd, util.DisplayMsgFromError(err))
+				return
+			}
+			err = o.validate()
+			if err != nil {
+				util.WriteOut(cmd, util.DisplayMsgFromError(err))
+				return
+			}
+			err = o.run()
+			if err != nil {
+				util.WriteOut(cmd, util.DisplayMsgFromError(err))
+				return
+			}
+		},
 	}
 
-	addAddrFlag(cmd, &createAssetOpts.addr, addrHelp)
-	addFilesFlag(cmd, &createAssetOpts.files, fileHelp)
-
-	cmd.Flags().StringVar(&createAssetOpts.image, imageFlag, "", imageUsage)
+	o.addFlags(cmd)
 
 	return cmd
 }
 
-func runCreateAsset(cmd *cobra.Command, args []string) {
+// addFlags adds the necessary flags to the cobra.Command instance that will
+// execute
+func (o *CreateAssetOptions) addFlags(cmd *cobra.Command) {
+	addAddrFlag(cmd, &o.addr, addrHelp)
 
-	// Create from files
-	if cmd.Flag(fileFull).Changed {
-		util.WarnArgsIgnore(args, "creating from file")
-		util.WarnFlagsIgnore(
-			cmd,
-			[]string{imageFlag},
-			"creating from file")
-		err := createFromFiles(
-			createAssetOpts.files,
-			createAssetOpts.addr,
-			resources.AssetKind)
-		if err != nil {
-			fmt.Printf("unable to create resources: %v\n", err)
-		}
-	} else {
-		if len(args) != 1 {
-			fmt.Printf("unable to create asset: expected name argument")
-			return
-		}
-		asset := &resources.AssetResource{
-			Name:  args[0],
-			Image: createAssetOpts.image,
-		}
+	cmd.Flags().StringVar(&o.image, imageFlag, "", imageUsage)
+}
 
-		ctx, cancel := context.WithTimeout(
-			context.Background(),
-			time.Second)
-		defer cancel()
-		err := client.CreateAsset(ctx, asset, createAssetOpts.addr)
-
-		if err != nil {
-			fmt.Printf("Unable to create asset: %v\n", err)
-			return
-		}
-		fmt.Printf("created asset with name '%v'\n", args[0])
+// complete fills any remaining information for the runner that is not specified
+// by the flags.
+func (o *CreateAssetOptions) complete(args []string) error {
+	if len(args) == 1 {
+		o.name = args[0]
 	}
+	return nil
+}
+
+// validate checks if the user options are compatible and the command can
+// be executed
+func (o *CreateAssetOptions) validate() error {
+	if o.name == "" {
+		return errdefs.InvalidArgumentWithMsg("please specify the asset name")
+	}
+	return nil
+}
+
+func (o *CreateAssetOptions) run() error {
+	asset := &resources.AssetResource{
+		Name:  o.name,
+		Image: o.image,
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Second)
+	defer cancel()
+
+	return client.CreateAsset(ctx, asset, o.addr)
 }
