1a168ecece5a7c525bd9e50496e1c26149327244
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-01-03 19:14:07 +0000

    feature: method to check if messages are compatible. (closes #121)

--- internal/reflection/message.go
+++ internal/reflection/message.go
@@ -2,12 +2,16 @@ package reflection
 
 import (
 	"github.com/DuarteMRAlves/maestro/internal/validate"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
 )
 
 // Message describes a grpc message
 type Message interface {
 	fullyQualifiedName() string
+	// Compatible verifies if two Messages are compatible, meaning fields with
+	// equal numbers have the same type.
+	Compatible(other Message) bool
 }
 
 type message struct {
@@ -18,10 +22,56 @@ func newMessage(desc *desc.MessageDescriptor) (Message, error) {
 	if ok, err := validate.ArgNotNil(desc, "desc"); !ok {
 		return nil, err
 	}
-	s := &message{desc: desc}
+	s := newMessageInternal(desc)
 	return s, nil
+}
+
+func newMessageInternal(desc *desc.MessageDescriptor) Message {
+	s := &message{desc: desc}
+	return s
 }
 
 func (s *message) fullyQualifiedName() string {
 	return s.desc.GetFullyQualifiedName()
+}
+
+func (s *message) Compatible(other Message) bool {
+	otherMsg, ok := other.(*message)
+	if !ok {
+		return false
+	}
+	return s.cmpFields(otherMsg)
+}
+
+func (s *message) cmpFields(o *message) bool {
+	for _, sField := range s.desc.GetFields() {
+		number := sField.GetNumber()
+		oField := o.desc.FindFieldByNumber(number)
+
+		// Ignore unmatched fields
+		if oField == nil {
+			continue
+		}
+
+		// Both fields must be repeatable or not repeatable
+		if sField.IsRepeated() != oField.IsRepeated() {
+			return false
+		}
+
+		sType := sField.GetType()
+		oType := oField.GetType()
+		// Two fields with the same number do not have the same type
+		if sType != oType {
+			return false
+		}
+		if sType == descriptor.FieldDescriptorProto_TYPE_MESSAGE {
+			sFieldMessage := newMessageInternal(sField.GetMessageType())
+			oFieldMessage := newMessageInternal(oField.GetMessageType())
+
+			if !sFieldMessage.Compatible(oFieldMessage) {
+				return false
+			}
+		}
+	}
+	return true
 }
