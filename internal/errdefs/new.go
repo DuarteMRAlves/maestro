b07dd6d2d282dc6ccfb29d7dc4d8b8365a52e6cf
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-01-05 17:19:37 +0000

    feature: prepend message to error while keeping type. (closes #124)

--- internal/errdefs/new.go
+++ internal/errdefs/new.go
@@ -78,3 +78,30 @@ func UnknownWithError(err error) error {
 func UnknownWithMsg(format string, a ...interface{}) error {
 	return UnknownWithError(fmt.Errorf(format, a...))
 }
+
+// PrependMsg returns a new error of the same type of the received error that
+// prepends a message to the received error message
+func PrependMsg(err error, format string, a ...interface{}) error {
+	format = fmt.Sprintf("%s: %%s", format)
+	a = append(a, err)
+
+	causeErr := getImplementer(err)
+	switch causeErr.(type) {
+	case AlreadyExists:
+		return AlreadyExistsWithMsg(format, a...)
+	case NotFound:
+		return NotFoundWithMsg(format, a...)
+	case InvalidArgument:
+		return InvalidArgumentWithMsg(format, a...)
+	case FailedPrecondition:
+		return FailedPreconditionWithMsg(format, a...)
+	case Unavailable:
+		return UnavailableWithMsg(format, a...)
+	case Internal:
+		return InternalWithMsg(format, a...)
+	case Unknown:
+		return UnknownWithMsg(format, a...)
+	default:
+		return UnknownWithMsg(format, a...)
+	}
+}
