fd0a5dd41c1cc3016d710972b3ebd44c30c86d26
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-01-28 16:45:52 +0000

    test: storage manager stage create and get tests. (closes #268) (#312)

--- internal/storage/manager_stage_test.go
+++ internal/storage/manager_stage_test.go
@@ -266,8 +266,8 @@ func testCreateStageError(
 			if err := helper.SaveAsset(a); err != nil {
 				return err
 			}
-			o := &api.Orchestration{Name: util.OrchestrationNameForNum(0)}
-			if err := helper.SaveOrchestration(o); err != nil {
+			o := &api.Stage{Name: util.StageNameForNum(0)}
+			if err := helper.SaveStage(o); err != nil {
 				return err
 			}
 			s := &api.Stage{Name: "duplicate"}
@@ -286,4 +286,317 @@ func testCreateStageError(
 	)
 	assert.Assert(t, assertErrTypeFn(err), "wrong error type")
 	assert.Equal(t, expectedErrMsg, err.Error(), "wrong error message")
+}
+
+func TestManager_GetMatchingStages(t *testing.T) {
+	tests := []struct {
+		name   string
+		req    *api.GetStageRequest
+		stored []*api.Stage
+		// names of the expected stages
+		expected []api.StageName
+	}{
+		{
+			name:     "zero elements stored, nil req",
+			req:      nil,
+			stored:   []*api.Stage{},
+			expected: []api.StageName{},
+		},
+		{
+			name:     "zero elements stored, some req",
+			req:      &api.GetStageRequest{Name: "some-name"},
+			stored:   []*api.Stage{},
+			expected: []api.StageName{},
+		},
+		{
+			name: "one element stored, nil req",
+			req:  nil,
+			stored: []*api.Stage{
+				testStage(0, api.StageSucceeded),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "one element stored, matching name req",
+			req:  &api.GetStageRequest{Name: util.StageNameForNum(0)},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "one element stored, non-matching name req",
+			req:  &api.GetStageRequest{Name: util.StageNameForNum(1)},
+			stored: []*api.Stage{
+				testStage(2, api.StagePending),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, nil req",
+			req:  nil,
+			stored: []*api.Stage{
+				testStage(1, api.StagePending),
+				testStage(5, api.StageSucceeded),
+				testStage(3, api.StageFailed),
+			},
+			expected: []api.StageName{
+				util.StageNameForNum(1),
+				util.StageNameForNum(3),
+				util.StageNameForNum(5),
+			},
+		},
+		{
+			name: "multiple elements stored, matching name req",
+			req:  &api.GetStageRequest{Name: util.StageNameForNum(2)},
+			stored: []*api.Stage{
+				testStage(3, api.StageRunning),
+				testStage(1, api.StagePending),
+				testStage(2, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(2)},
+		},
+		{
+			name: "multiple elements stored, non-matching name req",
+			req:  &api.GetStageRequest{Name: util.StageNameForNum(2)},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching phase req",
+			req: &api.GetStageRequest{
+				Phase: api.StageFailed,
+			},
+			stored: []*api.Stage{
+				testStage(1, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(0, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "multiple elements stored, non-matching phase req",
+			req: &api.GetStageRequest{
+				Phase: api.StageSucceeded,
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(2, api.StagePending),
+				testStage(1, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching service req",
+			req: &api.GetStageRequest{
+				Service: util.StageServiceForNum(2),
+			},
+			stored: []*api.Stage{
+				testStage(1, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(2, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(2)},
+		},
+		{
+			name: "multiple elements stored, non-matching service req",
+			req: &api.GetStageRequest{
+				Service: util.StageServiceForNum(4),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(2, api.StagePending),
+				testStage(1, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching rpc req",
+			req: &api.GetStageRequest{
+				Rpc: util.StageRpcForNum(0),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(2, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "multiple elements stored, non-matching rpc req",
+			req: &api.GetStageRequest{
+				Rpc: util.StageRpcForNum(2),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching address req",
+			req: &api.GetStageRequest{
+				Address: util.StageAddressForNum(1),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(1)},
+		},
+		{
+			name: "multiple elements stored, non-matching address req",
+			req: &api.GetStageRequest{
+				Address: util.StageAddressForNum(1),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(3, api.StagePending),
+				testStage(2, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching orchestration req",
+			req: &api.GetStageRequest{
+				Orchestration: util.OrchestrationNameForNum(0),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "multiple elements stored, non-matching orchestration req",
+			req: &api.GetStageRequest{
+				Orchestration: util.OrchestrationNameForNum(2),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(3, api.StagePending),
+				testStage(4, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+		{
+			name: "multiple elements stored, matching asset req",
+			req: &api.GetStageRequest{
+				Asset: util.AssetNameForNum(0),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StageRunning),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageFailed),
+			},
+			expected: []api.StageName{util.StageNameForNum(0)},
+		},
+		{
+			name: "multiple elements stored, non-matching asset req",
+			req: &api.GetStageRequest{
+				Asset: util.AssetNameForNum(2),
+			},
+			stored: []*api.Stage{
+				testStage(0, api.StagePending),
+				testStage(3, api.StagePending),
+				testStage(1, api.StageRunning),
+			},
+			expected: []api.StageName{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var received []*api.Stage
+
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				m, err := NewManager(NewTestContext(db))
+				assert.NilError(t, err, "manager creation")
+
+				for _, s := range test.stored {
+					err = db.Update(
+						func(txn *badger.Txn) error {
+							return saveStageAndDependencies(txn, s)
+						},
+					)
+				}
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						received, err = m.GetMatchingStage(
+							txn,
+							test.req,
+						)
+						return err
+					},
+				)
+				assert.NilError(t, err, "get orchestration")
+				assert.Equal(t, len(test.expected), len(received))
+
+				seen := make(map[api.StageName]bool, 0)
+				for _, e := range test.expected {
+					seen[e] = false
+				}
+
+				for _, r := range received {
+					alreadySeen, exists := seen[r.Name]
+					assert.Assert(t, exists, "element should be expected")
+					// Elements can't be seen twice
+					assert.Assert(t, !alreadySeen, "element already seen")
+					seen[r.Name] = true
+				}
+
+				for _, e := range test.expected {
+					// All elements should be seen
+					assert.Assert(t, seen[e], "element not seen")
+				}
+			},
+		)
+	}
+}
+
+func testStage(num int, phase api.StagePhase) *api.Stage {
+	return &api.Stage{
+		Name:          util.StageNameForNum(num),
+		Phase:         phase,
+		Service:       util.StageServiceForNum(num),
+		Rpc:           util.StageRpcForNum(num),
+		Address:       util.StageAddressForNum(num),
+		Orchestration: util.OrchestrationNameForNum(num),
+		Asset:         util.AssetNameForNum(num),
+	}
+}
+
+func saveStageAndDependencies(txn *badger.Txn, s *api.Stage) error {
+	helper := TxnHelper{txn: txn}
+	if !helper.ContainsOrchestration(s.Orchestration) {
+		err := helper.SaveOrchestration(
+			orchestrationForName(
+				s.Orchestration,
+				api.OrchestrationRunning,
+			),
+		)
+		if err != nil {
+			return err
+		}
+	}
+	if !helper.ContainsAsset(s.Asset) {
+		err := helper.SaveAsset(&api.Asset{Name: s.Asset})
+		if err != nil {
+			return err
+		}
+	}
+	return helper.SaveStage(s)
 }
