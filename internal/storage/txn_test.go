ec8111fc36906c2b15b022384d2a62090667119d
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-01-28 13:35:07 +0000

    test: TxnHelper save and load tested for Asset, Stage and Link. (closes #304) (#310)

--- internal/storage/txn_test.go
+++ internal/storage/txn_test.go
@@ -192,3 +192,389 @@ func TestTxnHelper_LoadOrchestration(t *testing.T) {
 		)
 	}
 }
+
+func TestTxnHelper_SaveStage(t *testing.T) {
+	tests := []struct {
+		name     string
+		stage    *api.Stage
+		expected []byte
+	}{
+		{
+			name: "default stage",
+			stage: &api.Stage{
+				Name:          "some-name",
+				Phase:         api.StagePending,
+				Service:       "",
+				Rpc:           "",
+				Address:       "",
+				Orchestration: "default",
+				Asset:         "",
+			},
+			expected: []byte("some-name;" + string(api.StagePending) + ";;;;default;"),
+		},
+		{
+			name: "non default stage",
+			stage: &api.Stage{
+				Name:          "some-name",
+				Phase:         api.StageRunning,
+				Service:       "SomeService",
+				Rpc:           "SomeRpc",
+				Address:       "SomeAddress",
+				Orchestration: "some-orchestration",
+				Asset:         "some-asset",
+			},
+			expected: []byte("some-name;" + string(api.StageRunning) + ";SomeService;SomeRpc;SomeAddress;some-orchestration;some-asset"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var stored []byte
+
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.SaveStage(test.stage)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						item, err := txn.Get(stageKey(test.stage.Name))
+						if err != nil {
+							return err
+						}
+						stored, err = item.ValueCopy(nil)
+						return err
+					},
+				)
+				assert.Equal(t, len(test.expected), len(stored), "stored size")
+				for i, e := range test.expected {
+					assert.Equal(t, e, stored[i], "stored not equal")
+				}
+			},
+		)
+	}
+}
+
+func TestTxnHelper_LoadStage(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected *api.Stage
+		stored   []byte
+	}{
+		{
+			name:   "default stage",
+			stored: []byte("some-name;" + string(api.StagePending) + ";;;;default;"),
+			expected: &api.Stage{
+				Name:          "some-name",
+				Phase:         api.StagePending,
+				Service:       "",
+				Rpc:           "",
+				Address:       "",
+				Orchestration: "default",
+				Asset:         "",
+			},
+		},
+		{
+			name:   "non default stage",
+			stored: []byte("some-name;" + string(api.StageRunning) + ";SomeService;SomeRpc;SomeAddress;some-orchestration;some-asset"),
+			expected: &api.Stage{
+				Name:          "some-name",
+				Phase:         api.StageRunning,
+				Service:       "SomeService",
+				Rpc:           "SomeRpc",
+				Address:       "SomeAddress",
+				Orchestration: "some-orchestration",
+				Asset:         "some-asset",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var loaded api.Stage
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						return txn.Set(
+							stageKey(test.expected.Name),
+							test.stored,
+						)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.LoadStage(
+							&loaded,
+							test.expected.Name,
+						)
+					},
+				)
+				assert.NilError(t, err, "load error")
+				assert.DeepEqual(t, test.expected, &loaded)
+			},
+		)
+	}
+}
+
+func TestTxnHelper_SaveLink(t *testing.T) {
+	tests := []struct {
+		name     string
+		link     *api.Link
+		expected []byte
+	}{
+		{
+			name: "default link",
+			link: &api.Link{
+				Name:        "some-name",
+				SourceStage: "",
+				SourceField: "",
+				TargetStage: "",
+				TargetField: "",
+			},
+			expected: []byte("some-name;;;;"),
+		},
+		{
+			name: "non default link",
+			link: &api.Link{
+				Name:        "some-name",
+				SourceStage: "source-stage",
+				SourceField: "SourceField",
+				TargetStage: "target-stage",
+				TargetField: "TargetField",
+			},
+			expected: []byte("some-name;source-stage;SourceField;target-stage;TargetField"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var stored []byte
+
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.SaveLink(test.link)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						item, err := txn.Get(linkKey(test.link.Name))
+						if err != nil {
+							return err
+						}
+						stored, err = item.ValueCopy(nil)
+						return err
+					},
+				)
+				assert.Equal(t, len(test.expected), len(stored), "stored size")
+				for i, e := range test.expected {
+					assert.Equal(t, e, stored[i], "stored not equal")
+				}
+			},
+		)
+	}
+}
+
+func TestTxnHelper_LoadLink(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected *api.Link
+		stored   []byte
+	}{
+		{
+			name:   "default link",
+			stored: []byte("some-name;;;;"),
+			expected: &api.Link{
+				Name:        "some-name",
+				SourceStage: "",
+				SourceField: "",
+				TargetStage: "",
+				TargetField: "",
+			},
+		},
+		{
+			name:   "non default link",
+			stored: []byte("some-name;source-stage;SourceField;target-stage;TargetField"),
+			expected: &api.Link{
+				Name:        "some-name",
+				SourceStage: "source-stage",
+				SourceField: "SourceField",
+				TargetStage: "target-stage",
+				TargetField: "TargetField",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var loaded api.Link
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						return txn.Set(
+							linkKey(test.expected.Name),
+							test.stored,
+						)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.LoadLink(
+							&loaded,
+							test.expected.Name,
+						)
+					},
+				)
+				assert.NilError(t, err, "load error")
+				assert.DeepEqual(t, test.expected, &loaded)
+			},
+		)
+	}
+}
+
+func TestTxnHelper_SaveAsset(t *testing.T) {
+	tests := []struct {
+		name     string
+		asset    *api.Asset
+		expected []byte
+	}{
+		{
+			name: "default asset",
+			asset: &api.Asset{
+				Name:  "some-name",
+				Image: "",
+			},
+			expected: []byte("some-name;"),
+		},
+		{
+			name: "non default stage",
+			asset: &api.Asset{
+				Name:  "some-name",
+				Image: "some-image",
+			},
+			expected: []byte("some-name;some-image"),
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var stored []byte
+
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.SaveAsset(test.asset)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						item, err := txn.Get(assetKey(test.asset.Name))
+						if err != nil {
+							return err
+						}
+						stored, err = item.ValueCopy(nil)
+						return err
+					},
+				)
+				assert.Equal(t, len(test.expected), len(stored), "stored size")
+				for i, e := range test.expected {
+					assert.Equal(t, e, stored[i], "stored not equal")
+				}
+			},
+		)
+	}
+}
+
+func TestTxnHelper_LoadAsset(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected *api.Asset
+		stored   []byte
+	}{
+		{
+			name:   "default asset",
+			stored: []byte("some-name;"),
+			expected: &api.Asset{
+				Name:  "some-name",
+				Image: "",
+			},
+		},
+		{
+			name:   "non default stage",
+			stored: []byte("some-name;some-image"),
+			expected: &api.Asset{
+				Name:  "some-name",
+				Image: "some-image",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				var loaded api.Asset
+				db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+				assert.NilError(t, err, "db creation")
+				defer db.Close()
+
+				err = db.Update(
+					func(txn *badger.Txn) error {
+						return txn.Set(
+							assetKey(test.expected.Name),
+							test.stored,
+						)
+					},
+				)
+				assert.NilError(t, err, "save error")
+
+				err = db.View(
+					func(txn *badger.Txn) error {
+						helper := NewTxnHelper(txn)
+						return helper.LoadAsset(
+							&loaded,
+							test.expected.Name,
+						)
+					},
+				)
+				assert.NilError(t, err, "load error")
+				assert.DeepEqual(t, test.expected, &loaded)
+			},
+		)
+	}
+}
