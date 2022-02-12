22910539a5f7193823e6d42b78abc6b3438f1c2e
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-02-12 16:55:07 +0000

    feature: set orchestration and stage to running when execution starts. (closes #364) (#405)

--- internal/exec/manager.go
+++ internal/exec/manager.go
@@ -3,6 +3,8 @@ package exec
 import (
 	"github.com/DuarteMRAlves/maestro/internal/api"
 	"github.com/DuarteMRAlves/maestro/internal/arch"
+	"github.com/DuarteMRAlves/maestro/internal/errdefs"
+	"github.com/DuarteMRAlves/maestro/internal/kv"
 	"github.com/DuarteMRAlves/maestro/internal/rpc"
 	"github.com/dgraph-io/badger/v3"
 	"sync"
@@ -38,14 +40,13 @@ func (m *manager) StartExecution(
 		name api.OrchestrationName
 		err  error
 	)
-
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
 	name = req.Orchestration
 	if name == "" {
 		name = arch.DefaultOrchestrationName
 	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
 
 	_, exists := m.executions[name]
 	// Already exists, do nothing.
@@ -56,6 +57,46 @@ func (m *manager) StartExecution(
 	m.executions[name], err = newBuilder(txn, m.reflectionManager).
 		withOrchestration(name).
 		build()
+	if err != nil {
+		return err
+	}
 
-	return err
+	err = m.setRunning(txn, name)
+	if err != nil {
+		return err
+	}
+
+	m.executions[name].Start()
+	return nil
+}
+
+func (m *manager) setRunning(
+	txn *badger.Txn,
+	name api.OrchestrationName,
+) error {
+	var err error
+	helper := kv.NewTxnHelper(txn)
+	o := &api.Orchestration{}
+	err = helper.LoadOrchestration(o, name)
+	if err != nil {
+		return errdefs.PrependMsg(err, "start execution")
+	}
+	o.Phase = api.OrchestrationRunning
+	err = helper.SaveOrchestration(o)
+	if err != nil {
+		return errdefs.PrependMsg(err, "start execution")
+	}
+	for _, sName := range o.Stages {
+		s := &api.Stage{}
+		err = helper.LoadStage(s, sName)
+		if err != nil {
+			return errdefs.PrependMsg(err, "start execution")
+		}
+		s.Phase = api.StageRunning
+		err = helper.SaveStage(s)
+		if err != nil {
+			return errdefs.PrependMsg(err, "start execution")
+		}
+	}
+	return nil
 }
