a7beed4b50f76fd1bb4e42deff34aa6272f4fe3f
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-02-14 20:39:42 +0000

    bug: maestro main with database creation. (closes #415)

--- internal/kv/db.go
+++ internal/kv/db.go
@@ -1,10 +1,20 @@
 package kv
 
 import (
+	"github.com/DuarteMRAlves/maestro/internal/errdefs"
 	"github.com/dgraph-io/badger/v3"
 	"gotest.tools/v3/assert"
 	"testing"
 )
+
+func NewDb() (*badger.DB, error) {
+	opts := badger.DefaultOptions("").WithInMemory(true)
+	db, err := badger.Open(opts)
+	if err != nil {
+		return nil, errdefs.InternalWithMsg("create database: %v", err)
+	}
+	return db, err
+}
 
 func NewTestDb(t *testing.T) *badger.DB {
 	opts := badger.DefaultOptions("").
