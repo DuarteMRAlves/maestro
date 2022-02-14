fbdde449e2724e066790f6e5a628f754a3162bac
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-02-14 20:30:45 +0000

    feature: add PubSub to Execution. (closes #421)

--- internal/exec/build.go
+++ internal/exec/build.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/DuarteMRAlves/maestro/internal/api"
 	"github.com/DuarteMRAlves/maestro/internal/errdefs"
+	"github.com/DuarteMRAlves/maestro/internal/events"
 	"github.com/DuarteMRAlves/maestro/internal/kv"
 	"github.com/DuarteMRAlves/maestro/internal/rpc"
 	"github.com/dgraph-io/badger/v3"
@@ -70,6 +71,7 @@ func (b *Builder) build() (*Execution, error) {
 	e := &Execution{
 		orchestration: b.orchestration,
 		stages:        b.stageMap,
+		pubSub:        events.NewPubSub(events.DefaultPubSubContext()),
 	}
 	return e, nil
 }
--- internal/exec/execution.go
+++ internal/exec/execution.go
@@ -2,6 +2,7 @@ package exec
 
 import (
 	"github.com/DuarteMRAlves/maestro/internal/api"
+	"github.com/DuarteMRAlves/maestro/internal/events"
 )
 
 // Execution executes an orchestration.
@@ -9,6 +10,7 @@ type Execution struct {
 	orchestration *api.Orchestration
 
 	stages *StageMap
+	pubSub *events.PubSub
 
 	term chan struct{}
 	errs chan error
@@ -31,9 +33,10 @@ func (e *Execution) Start() {
 			ch := make(chan struct{})
 			e.done = append(e.done, ch)
 			cfg := &RunCfg{
-				term: e.term,
-				errs: e.errs,
-				done: ch,
+				pubSub: e.pubSub,
+				term:   e.term,
+				errs:   e.errs,
+				done:   ch,
 			}
 			go s.Run(cfg)
 		},
@@ -51,4 +54,8 @@ func (e *Execution) Stop() {
 			s.Close()
 		},
 	)
+}
+
+func (e *Execution) Subscribe() ([]*api.Event, <-chan *api.Event) {
+	return e.pubSub.RegisterSub()
 }
