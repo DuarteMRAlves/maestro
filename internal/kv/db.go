644ba363c5f30910fcb34b8d5701f2b765c03ba8
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-02-28 17:28:19 +0000

    feature: badger logger with zap package. (closes #491)

--- internal/kv/db.go
+++ internal/kv/db.go
@@ -2,13 +2,21 @@ package kv
 
 import (
 	"github.com/DuarteMRAlves/maestro/internal/errdefs"
+	"github.com/DuarteMRAlves/maestro/internal/logs"
 	"github.com/dgraph-io/badger/v3"
+	"go.uber.org/zap"
 	"gotest.tools/v3/assert"
 	"testing"
 )
 
 func NewDb() (*badger.DB, error) {
-	opts := badger.DefaultOptions("").WithInMemory(true)
+	lvl := zap.NewAtomicLevelAt(zap.InfoLevel)
+	zapLogger, err := logs.DefaultProductionLogger(lvl)
+	if err != nil {
+		return nil, errdefs.InternalWithMsg("create database: %v", err)
+	}
+	logger := NewBadgerLogger(zapLogger.Sugar())
+	opts := badger.DefaultOptions("").WithInMemory(true).WithLogger(logger)
 	db, err := badger.Open(opts)
 	if err != nil {
 		return nil, errdefs.InternalWithMsg("create database: %v", err)
@@ -17,10 +25,39 @@ func NewDb() (*badger.DB, error) {
 }
 
 func NewTestDb(t *testing.T) *badger.DB {
+	lvl := zap.NewAtomicLevelAt(zap.WarnLevel)
+	zapLogger, err := logs.DefaultProductionLogger(lvl)
+	assert.NilError(t, err, "error creating db logger")
+	logger := NewBadgerLogger(zapLogger.Sugar())
 	opts := badger.DefaultOptions("").
 		WithInMemory(true).
-		WithLoggingLevel(badger.WARNING)
+		WithLoggingLevel(badger.WARNING).
+		WithLogger(logger)
 	db, err := badger.Open(opts)
 	assert.NilError(t, err, "error creating test db")
 	return db
+}
+
+type badgerLogger struct {
+	logger *zap.SugaredLogger
+}
+
+func NewBadgerLogger(logger *zap.SugaredLogger) badger.Logger {
+	return &badgerLogger{logger: logger}
+}
+
+func (b *badgerLogger) Errorf(s string, i ...interface{}) {
+	b.logger.Errorf(s, i...)
+}
+
+func (b *badgerLogger) Warningf(s string, i ...interface{}) {
+	b.logger.Warnf(s, i...)
+}
+
+func (b *badgerLogger) Infof(s string, i ...interface{}) {
+	b.logger.Infof(s, i...)
+}
+
+func (b *badgerLogger) Debugf(s string, i ...interface{}) {
+	b.logger.Debugf(s, i...)
 }
