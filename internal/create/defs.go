daf7c2ff5814c81e7a699f68d572164fbece1959
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-03-05 15:14:32 +0000

    refactor: create package with definitions. (closes #542) (#547)

---
+++ internal/create/defs.go
@@ -0,0 +1,21 @@
+package create
+
+import "github.com/DuarteMRAlves/maestro/internal/domain"
+
+type SaveAsset func(domain.Asset) domain.AssetResult
+type LoadAsset func(domain.AssetName) domain.AssetResult
+
+type SaveStage func(domain.Stage) domain.StageResult
+type LoadStage func(domain.StageName) domain.StageResult
+
+type SaveLink func(domain.Link) domain.LinkResult
+type LoadLink func(domain.LinkName) domain.LinkResult
+
+type AssetRequest struct {
+	Name  string
+	Image domain.OptionalString
+}
+
+type AssetResponse struct {
+	Err domain.OptionalError
+}
