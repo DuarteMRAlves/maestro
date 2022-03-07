9ecb04e55c91219458d11ecfd8d88637a75eaeb7
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-07 20:31:29 +0000

    fixed build error with fmt.

--- internal/invoke/service.go
+++ internal/invoke/service.go
@@ -1,6 +1,9 @@
 package invoke
 
-import "github.com/jhump/protoreflect/desc"
+import (
+	"github.com/DuarteMRAlves/maestro/internal/errdefs"
+	"github.com/jhump/protoreflect/desc"
+)
 
 type Service interface {
 	Methods() []MethodDescriptor
@@ -14,7 +17,7 @@ func (s service) Methods() []MethodDescriptor {
 	return s.methods
 }
 
-func newService(desc *desc.ServiceDescriptor) Service {
+func newService(desc *desc.ServiceDescriptor) (Service, error) {
 	methodDescriptors := desc.GetMethods()
 	methods := make([]MethodDescriptor, 0, len(methodDescriptors))
 
@@ -22,10 +25,11 @@ func newService(desc *desc.ServiceDescriptor) Service {
 		m, err := newMethodDescriptor(d)
 		// Should never happen
 		if err != nil {
-			panic(err)
+			err = errdefs.PrependMsg(err, "create service %s", desc.GetName())
+			return nil, err
 		}
 		methods = append(methods, m)
 	}
 
-	return service{methods: methods}
+	return service{methods: methods}, nil
 }
