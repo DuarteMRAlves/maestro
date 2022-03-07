57cf8fdce1622281a531e2c994a0af5c5f209610
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-07 20:25:15 +0000

    refactor: functional service. (closes #583)

---
+++ internal/invoke/service.go
@@ -0,0 +1,31 @@
+package invoke
+
+import "github.com/jhump/protoreflect/desc"
+
+type Service interface {
+	Methods() []MethodDescriptor
+}
+
+type service struct {
+	methods []MethodDescriptor
+}
+
+func (s service) Methods() []MethodDescriptor {
+	return s.methods
+}
+
+func newService(desc *desc.ServiceDescriptor) Service {
+	methodDescriptors := desc.GetMethods()
+	methods := make([]MethodDescriptor, 0, len(methodDescriptors))
+
+	for _, d := range methodDescriptors {
+		m, err := newMethodDescriptor(d)
+		// Should never happen
+		if err != nil {
+			panic(err)
+		}
+		methods = append(methods, m)
+	}
+
+	return service{methods: methods}
+}
