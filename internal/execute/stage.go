f08d0b84f61831455d266025b481ce428ab0e20a
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-10 18:10:24 +0000

    refactor: moved merge stage to new codebase. (closes #611)

--- internal/execute/stage.go
+++ internal/execute/stage.go
@@ -2,6 +2,7 @@ package execute
 
 import (
 	"context"
+	"github.com/DuarteMRAlves/maestro/internal/domain"
 	"github.com/DuarteMRAlves/maestro/internal/invoke"
 	"sync"
 	"time"
@@ -125,6 +126,116 @@ func (s *sinkStage) Run(ctx context.Context) error {
 		case <-s.input:
 		case <-ctx.Done():
 			return nil
+		}
+	}
+}
+
+type mergeStage struct {
+	// fields are the names of the fields of the generated message that should
+	// be filled with the collected messages.
+	fields []domain.MessageField
+	// inputs are the several input channels from which to collect the messages.
+	inputs []<-chan state
+	// output is the channel used to send messages to the downstream stage.
+	output chan<- state
+	// gen generates empty messages for the output type.
+	gen invoke.MessageGenerator
+	// currId is the current id being constructed.
+	currId id
+}
+
+func newMergeStage(
+	fields []domain.MessageField,
+	inputs []<-chan state,
+	output chan<- state,
+	gen invoke.MessageGenerator,
+) mergeStage {
+	return mergeStage{
+		fields: fields,
+		inputs: inputs,
+		output: output,
+		gen:    gen,
+		currId: 0,
+	}
+}
+
+func (s *mergeStage) Run(ctx context.Context) error {
+	var (
+		// partial is the current message being constructed.
+		partial   invoke.DynamicMessage
+		currState state
+		done      bool
+	)
+	// latest stores the most recent state received from any channel.
+	latest := make([]state, 0, len(s.inputs))
+	for i := 0; i < len(s.inputs); i++ {
+		latest = append(latest, emptyState)
+	}
+	fieldSetters := make([]invoke.FieldSetter, 0, len(s.fields))
+	for _, f := range s.fields {
+		fieldSetters = append(fieldSetters, invoke.NewFieldSetter(f))
+	}
+	for {
+		partial = s.gen()
+		// number of fields in the partial message that are set.
+		setFields := 0
+		for i, input := range s.inputs {
+			currState = latest[i]
+			if currState.id < s.currId {
+				currState, done = s.takeUntilCurrId(ctx, input)
+				if done {
+					close(s.output)
+					return nil
+				}
+				latest[i] = currState
+			}
+			// If the currState id is higher than the current id, we will never
+			// be able to construct the message for the current id. As such, we
+			// need to break this cycle, discard the work and move to the next
+			// iteration with the currState id.
+			if currState.id > s.currId {
+				s.currId = currState.id
+				break
+			}
+			res := fieldSetters[i](partial, currState.msg.GrpcMessage())
+			if res.IsError() {
+				return res.Error()
+			}
+			partial = res.Unwrap()
+			setFields++
+		}
+		// All fields from inputs were set. The message can be sent
+		if setFields == len(s.inputs) {
+			sendState := newState(s.currId, partial)
+			select {
+			case s.output <- sendState:
+			case <-ctx.Done():
+				close(s.output)
+				return nil
+			}
+			s.currId++
+			for i := 0; i < len(s.inputs); i++ {
+				latest[i] = emptyState
+			}
+		}
+	}
+}
+
+func (s *mergeStage) takeUntilCurrId(
+	ctx context.Context,
+	input <-chan state,
+) (state, bool) {
+	for {
+		select {
+		case st, more := <-input:
+			if !more {
+				return emptyState, true
+			}
+			if st.id >= s.currId {
+				return st, false
+			}
+		case <-ctx.Done():
+			return emptyState, true
 		}
 	}
 }
