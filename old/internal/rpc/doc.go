cbb001d8d6bbc6943de00da408677f26637ce05b
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-03-17 20:02:39 +0000

    refactor: remove old reflection code. (#615) (#703)

--- old/internal/rpc/doc.go
+++
@@ -1,4 +0,0 @@
-// Package rpc handles rpc request to inspect the necessary
-// grpc servers and invoke their methods.
-// It offers a client api that connects to a specific server and inspects it.
-package rpc
