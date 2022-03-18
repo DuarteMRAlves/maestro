9bf02374fa849ba4d15f2d4fb9491de895bdf6d4
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-18 08:24:25 +0000

    refactor: map table tests for internal pkg. (#701)

--- internal/asset_test.go
+++ internal/asset_test.go
@@ -7,35 +7,34 @@ import (
 )
 
 func TestNewAssetName(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		name  string
 		valid bool
 	}{
-		{"largeNameWithLetters", true},
-		{"125largeName1565WithNumbers123", true},
-		{"name-with-dashes-123-and-number-021", true},
-		{"name/with-dash/and_underscore:tag-123", true},
-		{"a", true},
-		{"", true},
-		{"name--double-dash", false},
-		{"/back-slash-start", false},
-		{"colon-end:", false},
-		{"under-score-and-back-slash_/image", false},
+		"letters":        {name: "simpleName", valid: true},
+		"numbers":        {name: "125name1565num123", valid: true},
+		"eq punct":       {name: "dashes-123-and-021", valid: true},
+		"diff punct":     {name: "name-dash/and_under:tag-123", valid: true},
+		"empty":          {name: "", valid: true},
+		"start punct":    {name: "/back-slash-start", valid: false},
+		"end punct":      {name: "colon-end:", valid: false},
+		"two eq punct":   {name: "name--double", valid: false},
+		"two diff punct": {name: "name_/image", valid: false},
 	}
-	for _, test := range tests {
+	for name, tc := range tests {
 		t.Run(
-			test.name,
+			name,
 			func(t *testing.T) {
-				created, err := NewAssetName(test.name)
-				if test.valid {
+				created, err := NewAssetName(tc.name)
+				if tc.valid {
 					assert.NilError(t, err, "create error")
-					assert.Equal(t, test.name, created.Unwrap())
+					assert.Equal(t, tc.name, created.Unwrap())
 				} else {
 					assert.Assert(t, created.IsEmpty())
 					var invalidIdentifier *InvalidIdentifier
 					assert.Assert(t, errors.As(err, &invalidIdentifier))
 					assert.Equal(t, "asset", invalidIdentifier.Type)
-					assert.Equal(t, test.name, invalidIdentifier.Ident)
+					assert.Equal(t, tc.name, invalidIdentifier.Ident)
 				}
 			},
 		)
