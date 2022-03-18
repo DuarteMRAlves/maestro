967a0e9739bbadfa84685d27fe38e3696939e646
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-18 16:53:16 +0000

    test: execution test with split and merge. (#692)

--- internal/execute/execution_test.go
+++ internal/execute/execution_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/DuarteMRAlves/maestro/internal"
 	"github.com/DuarteMRAlves/maestro/internal/mock"
+	"github.com/google/go-cmp/cmp"
 	"gotest.tools/v3/assert"
 	"sync"
 	"sync/atomic"
@@ -243,7 +244,7 @@ type linearSinkClient struct {
 	mu      sync.Mutex
 }
 
-func (s *linearSinkClient) Call(_ context.Context, req internal.Message) (
+func (c *linearSinkClient) Call(_ context.Context, req internal.Message) (
 	internal.Message,
 	error,
 ) {
@@ -251,18 +252,270 @@ func (s *linearSinkClient) Call(_ context.Context, req internal.Message) (
 	if !ok {
 		return nil, errors.New("sink request message is not *mock.Message")
 	}
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	// Receive while not at full capacity
-	if len(*s.collect) < s.max {
-		*s.collect = append(*s.collect, reqMock)
+	if len(*c.collect) < c.max {
+		*c.collect = append(*c.collect, reqMock)
 	}
 	// Notify when full. Remaining messages are discarded.
-	if len(*s.collect) == s.max && s.done != nil {
-		close(s.done)
-		s.done = nil
+	if len(*c.collect) == c.max && c.done != nil {
+		close(c.done)
+		c.done = nil
 	}
 	return &mock.Message{}, nil
 }
 
 func (c *linearSinkClient) Close() error { return nil }
+
+func TestExecution_SplitAndMerge(t *testing.T) {
+	fieldName := internal.NewMessageField("field")
+	originalField := internal.NewMessageField("original")
+	transformField := internal.NewMessageField("transform")
+
+	max := 3
+	collect := make([]*mock.Message, 0, max)
+	done := make(chan struct{})
+
+	emptyDesc := mock.MessageDescriptor{Ident: "empty"}
+	singleMsgDesc := mock.MessageDescriptor{Ident: "single"}
+	mergeMsgDesc := mock.MessageDescriptor{
+		Ident: "merge",
+		Fields: map[internal.MessageField]internal.MessageDesc{
+			originalField:  singleMsgDesc,
+			transformField: singleMsgDesc,
+		},
+	}
+
+	sourceMethod := mock.Method{
+		MethodClientBuilder: splitAndMergeSourceClientBuilder(fieldName),
+		In:                  emptyDesc,
+		Out:                 singleMsgDesc,
+	}
+	transformMethod := mock.Method{
+		MethodClientBuilder: splitAndMergeTransformClientBuilder(fieldName),
+		In:                  singleMsgDesc,
+		Out:                 singleMsgDesc,
+	}
+	sinkMethod := mock.Method{
+		MethodClientBuilder: splitAndMergeSinkClientBuilder(max, &collect, done),
+		In:                  mergeMsgDesc,
+		Out:                 emptyDesc,
+	}
+
+	sourceName := createStageName(t, "source")
+	transformName := createStageName(t, "transform")
+	sinkName := createStageName(t, "sink")
+
+	sourceAddr := internal.NewAddress("source")
+	transformAddr := internal.NewAddress("transform")
+	sinkAddr := internal.NewAddress("sink")
+
+	sourceContext := createMethodContext(sourceAddr)
+	transformContext := createMethodContext(transformAddr)
+	sinkContext := createMethodContext(sinkAddr)
+
+	sourceStage := internal.NewStage(sourceName, sourceContext)
+	transformStage := internal.NewStage(transformName, transformContext)
+	sinkStage := internal.NewStage(sinkName, sinkContext)
+
+	stages := map[internal.StageName]internal.Stage{
+		sourceName:    sourceStage,
+		transformName: transformStage,
+		sinkName:      sinkStage,
+	}
+	stageLoader := &mock.StageStorage{Stages: stages}
+
+	sourceToTransformName := createLinkName(t, "link-source-transform")
+	sourceToTransform := internal.NewLink(
+		sourceToTransformName,
+		internal.NewLinkEndpoint(sourceName, internal.NewEmptyMessageField()),
+		internal.NewLinkEndpoint(transformName, internal.NewEmptyMessageField()),
+	)
+
+	sourceToSinkName := createLinkName(t, "link-source-sink")
+	sourceToSink := internal.NewLink(
+		sourceToSinkName,
+		internal.NewLinkEndpoint(sourceName, internal.NewEmptyMessageField()),
+		internal.NewLinkEndpoint(
+			sinkName, internal.NewPresentMessageField(originalField),
+		),
+	)
+
+	transformToSinkName := createLinkName(t, "link-transform-sink")
+	transformToSink := internal.NewLink(
+		transformToSinkName,
+		internal.NewLinkEndpoint(transformName, internal.NewEmptyMessageField()),
+		internal.NewLinkEndpoint(
+			sinkName, internal.NewPresentMessageField(transformField),
+		),
+	)
+
+	links := map[internal.LinkName]internal.Link{
+		sourceToTransformName: sourceToTransform,
+		sourceToSinkName:      sourceToSink,
+		transformToSinkName:   transformToSink,
+	}
+	linkLoader := &mock.LinkStorage{Links: links}
+
+	methods := map[internal.MethodContext]internal.UnaryMethod{
+		sourceContext:    sourceMethod,
+		transformContext: transformMethod,
+		sinkContext:      sinkMethod,
+	}
+	methodLoader := &mock.MethodLoader{Methods: methods}
+
+	executionBuilder := NewBuilder(stageLoader, linkLoader, methodLoader)
+
+	orchestration := internal.NewOrchestration(
+		createOrchName(t, "orchestration"),
+		[]internal.StageName{sourceName, transformName, sinkName},
+		[]internal.LinkName{sourceToTransformName, sourceToSinkName, transformToSinkName},
+	)
+
+	e, err := executionBuilder(orchestration)
+	assert.NilError(t, err, "build error")
+
+	e.Start()
+	<-done
+	if err := e.Stop(); err != nil {
+		t.Fatalf("stop error: %s", err)
+	}
+	if diff := cmp.Diff(3, len(collect)); diff != "" {
+		t.Fatalf("mismatch on number of collected messages:\n%s", diff)
+	}
+	for i, msg := range collect {
+		counter := int64(i) + 1
+		origMsg := &mock.Message{
+			Fields: map[internal.MessageField]interface{}{fieldName: counter},
+		}
+		transfMsg := &mock.Message{
+			Fields: map[internal.MessageField]interface{}{fieldName: counter * 2},
+		}
+		expected := &mock.Message{
+			Fields: map[internal.MessageField]interface{}{
+				originalField:  origMsg,
+				transformField: transfMsg,
+			},
+		}
+		if diff := cmp.Diff(expected, msg); diff != "" {
+			t.Fatalf("mismatch on message %d:\n%s", i, diff)
+		}
+	}
+}
+
+func splitAndMergeSourceClientBuilder(field internal.MessageField) internal.UnaryClientBuilder {
+	return func(_ internal.Address) (internal.UnaryClient, error) {
+		c := &splitAndMergeSourceClient{
+			field:   field,
+			counter: 0,
+		}
+		return c, nil
+	}
+}
+
+type splitAndMergeSourceClient struct {
+	field   internal.MessageField
+	counter int64
+}
+
+func (c *splitAndMergeSourceClient) Call(
+	_ context.Context, req internal.Message,
+) (internal.Message, error) {
+	reqMock, ok := req.(*mock.Message)
+	if !ok {
+		return nil, errors.New("source request message is not *mock.Message")
+	}
+	if len(reqMock.Fields) != 0 {
+		return nil, errors.New("source request message is not empty")
+	}
+	val := atomic.AddInt64(&c.counter, 1)
+	repFields := map[internal.MessageField]interface{}{c.field: val}
+	return &mock.Message{Fields: repFields}, nil
+}
+
+func (c *splitAndMergeSourceClient) Close() error { return nil }
+
+func splitAndMergeTransformClientBuilder(field internal.MessageField) internal.UnaryClientBuilder {
+	return func(_ internal.Address) (internal.UnaryClient, error) {
+		c := &splitAndMergeTransformClient{field: field}
+		return c, nil
+	}
+}
+
+type splitAndMergeTransformClient struct {
+	field internal.MessageField
+}
+
+func (c *splitAndMergeTransformClient) Call(
+	_ context.Context, req internal.Message,
+) (internal.Message, error) {
+	reqMock, ok := req.(*mock.Message)
+	if !ok {
+		return nil, errors.New("transform request message is not *mock.Message")
+	}
+	val, ok := reqMock.Fields[c.field]
+	if !ok {
+		return nil, fmt.Errorf(
+			"transform request message does not have %s field",
+			c.field,
+		)
+	}
+	valAsInt64, ok := val.(int64)
+	if !ok {
+		return nil, fmt.Errorf(
+			"transform request message %s is not an int64",
+			c.field,
+		)
+	}
+	replyVal := 2 * valAsInt64
+	repFields := map[internal.MessageField]interface{}{c.field: replyVal}
+	return &mock.Message{Fields: repFields}, nil
+}
+
+func (c *splitAndMergeTransformClient) Close() error { return nil }
+
+func splitAndMergeSinkClientBuilder(
+	max int, collect *[]*mock.Message, done chan<- struct{},
+) internal.UnaryClientBuilder {
+	return func(_ internal.Address) (internal.UnaryClient, error) {
+		c := &splitAndMergeSinkClient{
+			max:     max,
+			collect: collect,
+			done:    done,
+			mu:      sync.Mutex{},
+		}
+		return c, nil
+	}
+}
+
+type splitAndMergeSinkClient struct {
+	max     int
+	collect *[]*mock.Message
+	done    chan<- struct{}
+	mu      sync.Mutex
+}
+
+func (c *splitAndMergeSinkClient) Call(_ context.Context, req internal.Message) (
+	internal.Message,
+	error,
+) {
+	reqMock, ok := req.(*mock.Message)
+	if !ok {
+		return nil, errors.New("sink request message is not *mock.Message")
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// Receive while not at full capacity
+	if len(*c.collect) < c.max {
+		*c.collect = append(*c.collect, reqMock)
+	}
+	// Notify when full. Remaining messages are discarded.
+	if len(*c.collect) == c.max && c.done != nil {
+		close(c.done)
+		c.done = nil
+	}
+	return &mock.Message{}, nil
+}
+
+func (c *splitAndMergeSinkClient) Close() error { return nil }
