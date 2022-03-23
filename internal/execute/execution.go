0deba2ae9a563b4a074955094a4d7dcaa86a44c1
DuarteMRAlves <duartemalves@tecnico.ulisboa.pt>
2022-03-23 10:14:12 +0000

    feature: add chan drainer to execution. (#739)

--- internal/execute/execution.go
+++ internal/execute/execution.go
@@ -3,21 +3,29 @@ package execute
 import (
 	"context"
 	"golang.org/x/sync/errgroup"
+	"time"
 )
 
 type execution struct {
 	stages *stageMap
+	chans  []chan state
 	wg     *errgroup.Group
 	cancel context.CancelFunc
 }
 
-func newExecution(stages *stageMap) *execution {
-	return &execution{stages: stages}
+func newExecution(stages *stageMap, chans []chan state) *execution {
+	return &execution{stages: stages, chans: chans}
 }
 
 func (e *execution) Start() {
 	ctx, cancel := context.WithCancel(context.Background())
 	wg, ctx := errgroup.WithContext(ctx)
+
+	chanDrainer := newChanDrainer(5*time.Millisecond, e.chans...)
+	wg.Go(func() error {
+		chanDrainer(ctx)
+		return nil
+	})
 
 	e.stages.iter(
 		func(s Stage) {
