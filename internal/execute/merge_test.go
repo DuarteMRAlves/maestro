30206608b205902edad78ecf121cd498515e1545
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-05-07 18:49:08 +0100

    refactor: remove mock message for merge test. (#814) (#817)

--- internal/execute/merge_test.go
+++ internal/execute/merge_test.go
@@ -2,10 +2,10 @@ package execute
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/DuarteMRAlves/maestro/internal"
-	"github.com/DuarteMRAlves/maestro/internal/mock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -13,12 +13,8 @@ func TestOfflineMergeStage_Run(t *testing.T) {
 	inner1 := internal.NewMessageField("inner1")
 	inner2 := internal.NewMessageField("inner2")
 	inner3 := internal.NewMessageField("inner3")
-	inner := []internal.MessageField{inner1, inner2, inner3}
 
-	f1 := internal.NewMessageField("f1")
-	f2 := internal.NewMessageField("f2")
-	f3 := internal.NewMessageField("f3")
-	fields := []internal.MessageField{f1, f2, f3}
+	fields := []internal.MessageField{inner1, inner2, inner3}
 
 	input1 := make(chan offlineState)
 	defer close(input1)
@@ -30,34 +26,40 @@ func TestOfflineMergeStage_Run(t *testing.T) {
 
 	output := make(chan offlineState)
 
-	s := newOfflineMerge(fields, inputs, output, mock.NewGen())
+	gen := func() internal.Message { return &testMergeOuterMessage{} }
+
+	s := newOfflineMerge(fields, inputs, output, gen)
+
+	inputs1 := []*testMergeInnerMessage{{1}, {4}, {7}, {10}}
+	inputs2 := []*testMergeInnerMessage{{2}, {5}, {8}, {11}}
+	inputs3 := []*testMergeInnerMessage{{3}, {6}, {9}, {12}}
 
 	expected := []offlineState{
-		newOfflineState(testMergeOuterMessage(inner, fields, 1)),
-		newOfflineState(testMergeOuterMessage(inner, fields, 2)),
-		newOfflineState(testMergeOuterMessage(inner, fields, 3)),
-		newOfflineState(testMergeOuterMessage(inner, fields, 6)),
+		newOfflineState(&testMergeOuterMessage{inputs1[0], inputs2[0], inputs3[0]}),
+		newOfflineState(&testMergeOuterMessage{inputs1[1], inputs2[1], inputs3[1]}),
+		newOfflineState(&testMergeOuterMessage{inputs1[2], inputs2[2], inputs3[2]}),
+		newOfflineState(&testMergeOuterMessage{inputs1[3], inputs2[3], inputs3[3]}),
 	}
 
 	go func() {
-		input1 <- newOfflineState(testMergeInnerMessage(inner[0], 1))
-		input1 <- newOfflineState(testMergeInnerMessage(inner[0], 2))
-		input1 <- newOfflineState(testMergeInnerMessage(inner[0], 3))
-		input1 <- newOfflineState(testMergeInnerMessage(inner[0], 6))
+		input1 <- newOfflineState(inputs1[0])
+		input1 <- newOfflineState(inputs1[1])
+		input1 <- newOfflineState(inputs1[2])
+		input1 <- newOfflineState(inputs1[3])
 	}()
 
 	go func() {
-		input2 <- newOfflineState(testMergeInnerMessage(inner[1], 1))
-		input2 <- newOfflineState(testMergeInnerMessage(inner[1], 2))
-		input2 <- newOfflineState(testMergeInnerMessage(inner[1], 3))
-		input2 <- newOfflineState(testMergeInnerMessage(inner[1], 6))
+		input2 <- newOfflineState(inputs2[0])
+		input2 <- newOfflineState(inputs2[1])
+		input2 <- newOfflineState(inputs2[2])
+		input2 <- newOfflineState(inputs2[3])
 	}()
 
 	go func() {
-		input3 <- newOfflineState(testMergeInnerMessage(inner[2], 1))
-		input3 <- newOfflineState(testMergeInnerMessage(inner[2], 2))
-		input3 <- newOfflineState(testMergeInnerMessage(inner[2], 3))
-		input3 <- newOfflineState(testMergeInnerMessage(inner[2], 6))
+		input3 <- newOfflineState(inputs3[0])
+		input3 <- newOfflineState(inputs3[1])
+		input3 <- newOfflineState(inputs3[2])
+		input3 <- newOfflineState(inputs3[3])
 	}()
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -75,7 +77,9 @@ func TestOfflineMergeStage_Run(t *testing.T) {
 
 	for i, exp := range expected {
 		out := <-output
-		cmpOpts := cmp.AllowUnexported(offlineState{})
+		cmpOpts := cmp.AllowUnexported(
+			offlineState{}, testMergeInnerMessage{}, testMergeOuterMessage{},
+		)
 		if diff := cmp.Diff(exp, out, cmpOpts); diff != "" {
 			t.Fatalf("mismatch on message %d:\n%s", i, diff)
 		}
@@ -88,12 +92,8 @@ func TestOnlineMergeStage_Run(t *testing.T) {
 	inner1 := internal.NewMessageField("inner1")
 	inner2 := internal.NewMessageField("inner2")
 	inner3 := internal.NewMessageField("inner3")
-	inner := []internal.MessageField{inner1, inner2, inner3}
 
-	f1 := internal.NewMessageField("f1")
-	f2 := internal.NewMessageField("f2")
-	f3 := internal.NewMessageField("f3")
-	fields := []internal.MessageField{f1, f2, f3}
+	fields := []internal.MessageField{inner1, inner2, inner3}
 
 	input1 := make(chan onlineState)
 	defer close(input1)
@@ -105,32 +105,39 @@ func TestOnlineMergeStage_Run(t *testing.T) {
 
 	output := make(chan onlineState)
 
-	s := newOnlineMerge(fields, inputs, output, mock.NewGen())
+	gen := func() internal.Message { return &testMergeOuterMessage{} }
+
+	s := newOnlineMerge(fields, inputs, output, gen)
+
+	inputs1 := []*testMergeInnerMessage{{1}, {4}, {7}, {10}}
+	inputs2 := []*testMergeInnerMessage{{2}, {5}, {8}, {11}}
+	inputs3 := []*testMergeInnerMessage{{3}, {6}, {9}, {12}}
 
 	expected := []onlineState{
-		newOnlineState(3, testMergeOuterMessage(inner, fields, 3)),
-		newOnlineState(6, testMergeOuterMessage(inner, fields, 6)),
+		// The messages with id 3 are at the indexes 2, 1, 1 for the inputs arrays.
+		newOnlineState(3, &testMergeOuterMessage{inputs1[2], inputs2[1], inputs3[1]}),
+		newOnlineState(6, &testMergeOuterMessage{inputs1[3], inputs2[3], inputs3[3]}),
 	}
 
 	go func() {
-		input1 <- newOnlineState(1, testMergeInnerMessage(inner[0], 1))
-		input1 <- newOnlineState(2, testMergeInnerMessage(inner[0], 2))
-		input1 <- newOnlineState(3, testMergeInnerMessage(inner[0], 3))
-		input1 <- newOnlineState(6, testMergeInnerMessage(inner[0], 6))
+		input1 <- newOnlineState(1, inputs1[0])
+		input1 <- newOnlineState(2, inputs1[1])
+		input1 <- newOnlineState(3, inputs1[2])
+		input1 <- newOnlineState(6, inputs1[3])
 	}()
 
 	go func() {
-		input2 <- newOnlineState(2, testMergeInnerMessage(inner[1], 2))
-		input2 <- newOnlineState(3, testMergeInnerMessage(inner[1], 3))
-		input2 <- newOnlineState(5, testMergeInnerMessage(inner[1], 5))
-		input2 <- newOnlineState(6, testMergeInnerMessage(inner[1], 6))
+		input2 <- newOnlineState(2, inputs2[0])
+		input2 <- newOnlineState(3, inputs2[1])
+		input2 <- newOnlineState(5, inputs2[2])
+		input2 <- newOnlineState(6, inputs2[3])
 	}()
 
 	go func() {
-		input3 <- newOnlineState(1, testMergeInnerMessage(inner[2], 2))
-		input3 <- newOnlineState(3, testMergeInnerMessage(inner[2], 3))
-		input3 <- newOnlineState(5, testMergeInnerMessage(inner[2], 5))
-		input3 <- newOnlineState(6, testMergeInnerMessage(inner[2], 6))
+		input3 <- newOnlineState(1, inputs3[0])
+		input3 <- newOnlineState(3, inputs3[1])
+		input3 <- newOnlineState(5, inputs3[2])
+		input3 <- newOnlineState(6, inputs3[3])
 	}()
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -148,7 +155,9 @@ func TestOnlineMergeStage_Run(t *testing.T) {
 
 	for i, exp := range expected {
 		out := <-output
-		cmpOpts := cmp.AllowUnexported(onlineState{})
+		cmpOpts := cmp.AllowUnexported(
+			onlineState{}, testMergeInnerMessage{}, testMergeOuterMessage{},
+		)
 		if diff := cmp.Diff(exp, out, cmpOpts); diff != "" {
 			t.Fatalf("mismatch on message %d:\n%s", i, diff)
 		}
@@ -157,20 +166,41 @@ func TestOnlineMergeStage_Run(t *testing.T) {
 	<-done
 }
 
-func testMergeOuterMessage(
-	inner, fields []internal.MessageField,
-	val int32,
-) *mock.Message {
-	msgFields := map[internal.MessageField]interface{}{}
-	for i, f := range fields {
-		innerMsg := testMergeInnerMessage(inner[i], val)
-		msgFields[f] = innerMsg
-	}
-	msg := &mock.Message{Fields: msgFields}
-	return msg
+type testMergeInnerMessage struct{ val int32 }
+
+func (m *testMergeInnerMessage) SetField(_ internal.MessageField, _ internal.Message) error {
+	panic("Should not set field for inner message in merge test")
 }
 
-func testMergeInnerMessage(field internal.MessageField, val int32) *mock.Message {
-	fields := map[internal.MessageField]interface{}{field: val}
-	return &mock.Message{Fields: fields}
+func (m *testMergeInnerMessage) GetField(_ internal.MessageField) (internal.Message, error) {
+	panic("Should not get field for inner message in merge test")
+}
+
+type testMergeOuterMessage struct {
+	inner1 *testMergeInnerMessage
+	inner2 *testMergeInnerMessage
+	inner3 *testMergeInnerMessage
+}
+
+func (m *testMergeOuterMessage) SetField(f internal.MessageField, v internal.Message) error {
+	inner, ok := v.(*testMergeInnerMessage)
+	if !ok {
+		panic("Set field for merge outer message did not receive inner message")
+	}
+	switch f.Unwrap() {
+	case "inner1":
+		m.inner1 = inner
+	case "inner2":
+		m.inner2 = inner
+	case "inner3":
+		m.inner3 = inner
+	default:
+		msg := fmt.Sprintf("Set field for merge outer message received unknown field: %s", f.Unwrap())
+		panic(msg)
+	}
+	return nil
+}
+
+func (m *testMergeOuterMessage) GetField(_ internal.MessageField) (internal.Message, error) {
+	panic("Should not get field for outer message in merge test")
 }
