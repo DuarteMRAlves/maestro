88e9a7236516e02fe213325aac7415cc6487d0db
Duarte Alves <47822147+DuarteMRAlves@users.noreply.github.com>
2022-05-07 11:20:05 +0100

    feature: offline and online execution structs. (#804) (#811)

--- internal/execute/execution.go
+++ internal/execute/execution.go
@@ -15,46 +15,94 @@ type Stage interface {
 	Run(context.Context) error
 }
 
-type Execution struct {
-	stages      *stageMap
-	chanDrainer chanDrainer
-	wg          *errgroup.Group
-	cancel      context.CancelFunc
+// Execution is an instantiation of a pipeline.
+type Execution interface {
+	Start()
+	Stop() error
+}
+
+type offlineExecution struct {
+	stages *stageMap
+
+	runner *runner
 
 	logger Logger
 }
 
-func newExecution(stages *stageMap, drainFunc chanDrainer, logger Logger) *Execution {
-	return &Execution{stages: stages, chanDrainer: drainFunc, logger: logger}
+func newOfflineExecution(stages *stageMap, logger Logger) *offlineExecution {
+	return &offlineExecution{stages: stages, logger: logger}
 }
 
-func (e *Execution) Start() {
-	ctx, cancel := context.WithCancel(context.Background())
-	wg, ctx := errgroup.WithContext(ctx)
+func (e *offlineExecution) Start() {
+	e.runner = newRunner()
 
-	wg.Go(func() error {
+	e.stages.iter(func(s Stage) {
+		e.runner.goWithCtx(s.Run)
+	})
+
+	e.logger.Debugf("Execution started\n")
+}
+
+func (e *offlineExecution) Stop() error {
+	err := e.runner.cancelAndWait()
+	e.logger.Debugf("Execution stopped\n")
+	return err
+}
+
+type onlineExecution struct {
+	stages      *stageMap
+	chanDrainer chanDrainer
+
+	runner *runner
+
+	logger Logger
+}
+
+func newOnlineExecution(stages *stageMap, drainFunc chanDrainer, logger Logger) *onlineExecution {
+	return &onlineExecution{stages: stages, chanDrainer: drainFunc, logger: logger}
+}
+
+func (e *onlineExecution) Start() {
+	e.runner = newRunner()
+
+	e.runner.goWithCtx(func(ctx context.Context) error {
 		e.chanDrainer(ctx)
 		return nil
 	})
 
-	e.stages.iter(
-		func(s Stage) {
-			wg.Go(
-				func() error {
-					return s.Run(ctx)
-				},
-			)
-		},
-	)
+	e.stages.iter(func(s Stage) {
+		e.runner.goWithCtx(s.Run)
+	})
 
-	e.cancel = cancel
-	e.wg = wg
 	e.logger.Debugf("Execution started\n")
 }
 
-func (e *Execution) Stop() error {
-	e.cancel()
-	err := e.wg.Wait()
+func (e *onlineExecution) Stop() error {
+	err := e.runner.cancelAndWait()
 	e.logger.Debugf("Execution stopped\n")
 	return err
+}
+
+type runner struct {
+	wg *errgroup.Group
+
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+}
+
+func newRunner() *runner {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg, ctx := errgroup.WithContext(ctx)
+	return &runner{wg: wg, cancelFunc: cancel, ctx: ctx}
+}
+
+func (r *runner) goWithCtx(f func(context.Context) error) {
+	r.wg.Go(func() error {
+		return f(r.ctx)
+	})
+}
+
+func (r *runner) cancelAndWait() error {
+	r.cancelFunc()
+	return r.wg.Wait()
 }
